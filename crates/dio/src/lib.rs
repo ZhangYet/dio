da9274bd9b7bde4bfceb1db9a3c6d76cb44714f1
Zhang Ye <ye.zhang@shopee.com>
2021-03-22 18:28:59 +0800

    finish part 7

--- crates/dio/src/lib.rs
+++ crates/dio/src/lib.rs
@@ -1,12 +1,21 @@
 mod binding_def;
+mod env;
 mod expr;
 mod stmt;
+mod utils;
 mod val;
 
-mod env;
-mod utils;
+pub use env::Env;
+pub use val::Val;
 
+#[derive(Debug)]
 pub struct Parse(stmt::Stmt);
+
+impl Parse {
+    pub fn eval(&self, env: &mut Env) -> Result<Val, String> {
+        self.0.eval(env)
+    }
+}
 
 pub fn parse(s: &str) -> Result<Parse, String> {
     let (s, stmt) = stmt::Stmt::new(s)?;
